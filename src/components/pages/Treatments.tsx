dffcaa412456e993be47b5144aa0de4ba4a5d994
AlwinTheChen <ac3116735555@gmail.com>
2025-08-05 00:09:15 +0100

    Updated Treatments.tsx

--- src/components/pages/Treatments.tsx
+++ src/components/pages/Treatments.tsx
@@ -179,7 +179,7 @@ export function Treatments() {
       ],
       image: '/assets/luxuryandlymphaticfacialcombo.jpg',
       category: 'combinations'
-      freshaLink: 'https://www.fresha.com/a/rejuvenating-touch-cardiff-94-woodville-road-w5mmt0z3?service=p%3A566687'
+      freshaLink: 'https://www.fresha.com/a/rejuvenating-touch-cardiff-94-woodville-road-w5mmt0z3?service=p%3A566688'
     },
     {
       id: '9',
