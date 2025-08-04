0cdb10a0bfb14f34cc907e604350d95a9735e5ec
AlwinTheChen <ac3116735555@gmail.com>
2025-08-05 00:24:43 +0100

    Updated Treatments.tsx

--- src/components/pages/Treatments.tsx
+++ src/components/pages/Treatments.tsx
@@ -528,7 +528,7 @@ export function Treatments() {
       ],
       image: '/assets/lipwaxing.jpg',
       category: 'waxing',
-      freshaLink: 'https://placeholder.com'
+      freshaLink: 'https://www.fresha.com/a/rejuvenating-touch-cardiff-94-woodville-road-w5mmt0z3?service=s%3A17805566'
     },
     {
       id: '26',
